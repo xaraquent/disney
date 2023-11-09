b50314b30040d07ca93187a4742784a885f3bdba
Simon <simon.michael.te15d@gmail.com>
2023-11-09 20:57:33 +0100

    done

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,56 @@
-import Image from 'next/image'
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    return (
+        <div
+            className='bg-cover bg-no-repeat bg-center w-full h-full grid grid-cols-2 gap-4'
+            style={{ backgroundImage: "url('/pics/disney-plus-1280-x-800-background-n41mbrw9vi6ivrqj.jpg')" }}
+        >
+            <div className='pl-16 pb-4 flex flex-col justify-between '>
+                <div></div>
+                <div className='grid gap-7'>
+                    <img src='/pics/Disney_Plus_logo.svg.png' alt='Your Image Description' class='w-auto h-20' />
+                    <h1 className='text-white text-5xl font-semibold'>
+                        The best stories in the world, all in one place.
+                    </h1>
+                    <div className='grid grid-cols-2 gap-10'>
+                        <div className='grid gap-4'>
+                            <div className='flex items-end'>
+                                <h1 className='text-white text-4xl font-semibold'>$89.99</h1>
+                                <p className='text-gray-400'>/year</p>
+                            </div>
+                            <p className='text-gray-400'>Save with an annual subscription. T&Cs apply.</p>
+                            <button class='bg-blue-500 hover:bg-blue-700 text-white py-3 px-10 rounded-sm'>
+                                WATCH 7-DAYS FOR FREE
+                            </button>
+                        </div>
+                        <div className='grid gap-4'>
+                            <div className='flex items-end'>
+                                <h1 className='text-white text-4xl font-semibold'>$8.99</h1>
+                                <p className='text-gray-400'>/month</p>
+                            </div>
+                            <p className='text-gray-400'>Start streaming Disney+. No ads, no up-charges.</p>
+                            <button class='bg-blue-500 hover:bg-blue-700 text-white py-3 px-10 rounded-sm'>
+                                WATCH 7-DAYS FOR FREE
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex items-center justify-start'>
+                    <img src='/pics/disney-white.png' alt='Your Image Description' className='mx-1 w-auto h-7' />
+                    <img src='/pics/plus-icon.png' alt='Your Image Description' class='mx-1 w-auto h-7' />
+                    <img src='/pics/pixar.png' alt='Your Image Description' class='mx-1 w-auto h-7' />
+                    <img src='/pics/plus-icon.png' alt='Your Image Description' class='mx-1 w-auto h-7' />
+                    <img
+                        src='/pics/marvel-logo-png-transparent.png'
+                        alt='Your Image Description'
+                        class='mx-1 w-auto h-7'
+                    />
+                    <img src='/pics/plus-icon.png' alt='Your Image Description' class='mx-1 w-auto h-7' />
+                    <img src='/pics/star-wars.png' alt='Your Image Description' class='mx-1 w-auto h-7' />
+                    <img src='/pics/plus-icon.png' alt='Your Image Description' class='mx-1 w-auto h-7' />
+                    <img src='/pics/national-geo.png' alt='Your Image Description' class='mx-1 w-auto h-7' />
+                </div>
+            </div>
+            <div></div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    );
 }
